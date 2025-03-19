36f4ce61905d134c04c2706eb6ddc0053c114472
kkirshenko <164340961+kkirshenko@users.noreply.github.com>
2025-03-20 00:54:05 +0300

    Update 0202_class.py

--- 0202_class.py
+++ 0202_class.py
@@ -1,6 +1,6 @@
 class Snow:
     def __init__(self, count: int):
-        self.__count = count
+        self.__count = count  # Инкапсуляция
 
     # Перегрузка оператора сложения
     def __add__(self, n: int):
